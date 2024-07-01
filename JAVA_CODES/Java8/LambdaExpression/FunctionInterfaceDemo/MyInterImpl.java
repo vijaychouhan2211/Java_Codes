6a438c955593e77fbd6b73e5fb8434c2500fad3a
vijaychouhan2211 <vijaychouhan16599@gmail.com>
2024-07-01 15:33:40 +0530

    update java8 files

--- JAVA_CODES/Java8/LambdaExpression/FunctionInterfaceDemo/MyInterImpl.java
+++
@@ -1,7 +0,0 @@
-package Java8.LambdaExpression.FunctionInterfaceDemo;
-
-public class MyInterImpl implements Myinter {
-    public void sayHello(){
-        System.out.println("I am From MyInterImpl");
-    }
-}
---
+++ JAVA_CODES/Java8/SumByInterface/SumInter.java
@@ -0,0 +1,6 @@
+package Java8.SumByInterface;
+
+@FunctionalInterface
+public interface SumInter {
+    public abstract int sum(int a, int b);
+}
